e4630dbc023f157580bb6dcbbde8b845b832730f
Sesirajnaidu <sesirajn@gmail.com>
2022-11-25 13:22:54 +0530

    post method

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=5000
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.post("/newData",(req,res)=>{
+    //console.log(req.body)
+    const {name,password}=req.body
+    console.log(name,password)
+    res.send("added")
+})
+
+app.get("/", (req,res)=>{
+ res.send("hi there today is thursday")
+})
+
+app.listen(port,()=>console.log("server 5000 is running"))
